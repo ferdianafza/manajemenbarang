2f1ba989f30cde8c806f84c8894e7e64bbc35ae0
HP <ferdian@ubuntu.ubuntu-domain>
2023-11-09 23:05:58 +0700

    tambah kolom stok di tabel barang

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_055908) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_155248) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -58,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_055908) do
     t.string "nama_barang"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stok"
   end
 
   create_table "staffs", force: :cascade do |t|
