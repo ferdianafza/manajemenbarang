6dedbad6c7a8b313b61845ca18f0097127eda80f
HP <ferdian@ubuntu.ubuntu-domain>
2023-11-09 23:34:10 +0700

    menambahkan fitur filter barang masuk

--- app/admin/barang_masuks.rb
+++ app/admin/barang_masuks.rb
@@ -1,35 +1,37 @@
 ActiveAdmin.register BarangMasuk do
-   menu label: "BarangMasuk"
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :barang, :barang_id, :jumlah, :waktu
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:barang, :jumlah, :waktu]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  menu label: "BarangMasuk"
+
+  permit_params :barang_id, :jumlah, :waktu
 
   form do |f|
     f.inputs do
-      f.input :barang, :label => 'Barang', :as => :select, :collection => Barang.all.map{|u| ["#{u.nama_barang}", u.id]}
-      # f.input :user_id, :label => 'User', :as => :select, :collection => User.all.map{|u| ["#{u.email}", u.id]}
+      f.input :barang, :label => 'Barang', :as => :select, :collection => Barang.all.map{|u| [u.nama_barang, u.id]}
       f.input :jumlah
-
       f.input :waktu, as: :datepicker,
                       datepicker_options: {
                         min_date: "-3D",
-                        max_date: "Date.today.to_date"
+                        max_date: Date.today.to_date
                       }
-
     end
     f.actions
   end
+
+  index do
+    selectable_column
+    column :id
+    column :barang do |barang_masuk|
+      barang_masuk.barang.nama_barang
+    end
+    column :jumlah
+    column :waktu
+    column :created_at
+    actions
+  end
+
+  filter :barang, collection: proc { Barang.all.map{|u| [u.nama_barang, u.id]} }
+  filter :jumlah
+  filter :waktu
+  filter :created_at
 
   controller do
     def create
@@ -49,11 +51,10 @@ ActiveAdmin.register BarangMasuk do
       barang.stok ||= 0
 
       # Tambahkan stok sesuai jumlah
-      barang.stok += jumlah
+      barang.stok = barang.stok.to_i + jumlah.to_i
 
       # Simpan perubahan
       barang.save
     end
   end
-
 end
